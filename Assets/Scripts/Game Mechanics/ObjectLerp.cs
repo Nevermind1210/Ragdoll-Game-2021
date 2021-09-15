21fe0cf57cbe778f874f343067619fbbbce116d8
Xavier Arias <xavier_rate@outlook.com>
2021-09-15 18:15:15 +1000

    Re-structing the level... two levels now and I'm changing the gameplay all over

---
+++ Assets/Scripts/Game Mechanics/ObjectLerp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game_Mechanics
+{
+    public class ObjectLerp : MonoBehaviour
+    {
+        [SerializeField] private Vector3 pointA = new Vector3(0, 0, 0);
+        [SerializeField] private Vector3 pointB = new Vector3(1, 1, 1);
+
+        private void Update()
+        {
+            // Super smooth movement between two points with an object!
+            transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+        }
+    }
+}
---
+++ Assets/Scripts/Game Mechanics/ScorePizza.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScorePizza : MonoBehaviour
+{
+  [Header("Score Count")] 
+  [SerializeField] private int score;
+  [SerializeField] private TextMeshProUGUI scoreText;
+  [Header("Timer")] 
+  [SerializeField] private float time;
+  [SerializeField] private TextMeshProUGUI timeText;
+
+  
+}
